20d3e6d64c3ffaf50612699d1c822bb13f4f4a4e
ikkentim <timpotze@gmail.com>
2015-04-13 11:21:21 +0200

    Added basic implementation of Karo.Common

---
+++ src/Karo.Common/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Karo.Common
+{
+    /// <summary>
+    /// Contains methods that define a player.
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// Is called when the turn of this <see cref="IPlayer"/> has started.
+        /// </summary>
+        /// <param name="previousMove">The previous move.</param>
+        /// <param name="done">A method to be called when the move has been calculated.</param>
+        void DoMove(Move previousMove, Func<Move> done);
+    }
+}
