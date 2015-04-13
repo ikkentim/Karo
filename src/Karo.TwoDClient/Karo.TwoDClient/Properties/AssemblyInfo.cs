e2da3b8a4bfb0aaec825bd8c7088a096cfc3af18
Luuk Holleman <luukholleman@gmail.com>
2015-04-13 12:03:45 +0200

    Added karo 2d xna client

--- src/Karo.TwoDClient/Karo.TwoDClient/Properties/AssemblyInfo.cs
+++ src/Karo.TwoDClient/Karo.TwoDClient/Properties/AssemblyInfo.cs
@@ -5,22 +5,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Karo.2DClient")]
+[assembly: AssemblyTitle("Karo.TwoDClient")]
+[assembly: AssemblyProduct("Karo.TwoDClient")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Karo.2DClient")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1b31450-2a4e-4ee3-808b-470dca3fbafe")]
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("adf7139f-583c-4479-b266-21f7be51383e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
