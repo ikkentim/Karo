ccc80fb2d7f03e6df1ffbdcd6afb44cca3c04b4b
Luuk Holleman <luukholleman@gmail.com>
2015-04-20 11:49:57 +0200

    Revert last commit

--- src/Karo.Core/Karo.h
+++ src/Karo.Core/Karo.h
@@ -1,7 +1,10 @@
 
 #include "Tile.h"
 #include "Piece.h"
+
 using namespace System;
+using namespace System::Collections::Generic;
+using namespace Karo::Common;
 
 namespace Karo {
     namespace Core {
@@ -10,11 +13,21 @@ namespace Karo {
         {
         public:
             Karo();
-            array<Tile^>^ GetTiles() {
-                return _tiles;
+            property IEnumerable<Tile^>^ Tiles {
+                IEnumerable<Tile^>^ get();
             }
+            property IEnumerable<Piece^>^ Pieces {
+                IEnumerable<Piece^>^ get();
+            }
+            property bool IsFinished {
+                bool get();
+            };
+            IEnumerable<Move^>^ GetAvailableMoves();
+            bool IsValidMove(Move^ move);
+            Karo^ WithMoveApplied(Move^ move);
         private:
-            array<Tile^> ^ _tiles;
+            array<Tile^>^ _tiles;
+            array<Piece^>^ _pieces;
         };
     }
 }
