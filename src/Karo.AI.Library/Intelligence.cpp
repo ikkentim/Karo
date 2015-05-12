5a35f6c3dfb5b53b4ca76ae6054fcb4efb27f731
Luuk Holleman <luukholleman@gmail.com>
2015-05-12 12:05:58 +0200

    Tmp fix

--- src/Karo.AI.Library/Intelligence.cpp
+++ src/Karo.AI.Library/Intelligence.cpp
@@ -18,7 +18,7 @@ void Intelligence::apply_move(BoardMove move, BoardPlayer player) {
 }
 BoardMove Intelligence::choose_best_move(int time, BoardPlayer player) {
     // TODO: Dismiss time for now, just go on for a few rounds.
-	return choose_best_move(state_, 12, player);
+	return choose_best_move(state_, 5, player);
 }
 
 BoardMove Intelligence::choose_best_move(BoardState * state, int time, BoardPlayer player) {
