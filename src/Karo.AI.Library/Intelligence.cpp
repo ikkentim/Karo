361b84ba4f2fa5c3d96ba1767381b0ef93845c11
Luuk Holleman <luukholleman@gmail.com>
2015-05-12 13:41:00 +0200

    Fixed bug where evaluation would not be zero sum on killer moves

--- src/Karo.AI.Library/Intelligence.cpp
+++ src/Karo.AI.Library/Intelligence.cpp
@@ -76,15 +76,18 @@ int Intelligence::evaluate(BoardState * state, BoardPlayer player) {
 
         BoardTile tile = allPieces[i].tile;
 
-#define TRY_OFFSET(ox,oy, idx); int idx = state->row_length(tile.x, tile.y, (ox), (oy), player) + \
+#define TRY_OFFSET(ox,oy, idx, oidx); int idx = state->row_length(tile.x, tile.y, (ox), (oy), player) + \
             1 + \
             state->row_length(tile.x, tile.y, -(ox), -(oy), player); \
-            if(idx >= 4) { return mypiece ? MAX_SCORE : MIN_SCORE; } else { score += mypiece ? idx : -idx; }
+			int oidx = state->row_length(tile.x, tile.y, (ox), (oy), OPPONENT(player)) + \
+            1 + \
+            state->row_length(tile.x, tile.y, -(ox), -(oy), OPPONENT(player)); \
+            if(idx >= 4) { return MAX_SCORE; } else if (oidx >= 4) { return MIN_SCORE; } else { score += mypiece ? idx : -idx; }
 
-        TRY_OFFSET(1, 1, d1);
-        TRY_OFFSET(1, 0, d2);
-        TRY_OFFSET(0, 1, d3);
-        TRY_OFFSET(-1, 1, d4);
+        TRY_OFFSET(1, 1, d1, od1);
+        TRY_OFFSET(1, 0, d2, od2);
+        TRY_OFFSET(0, 1, d3, od3);
+        TRY_OFFSET(-1, 1, d4, od4);
 
 #undef TRY_OFFSET
     }
