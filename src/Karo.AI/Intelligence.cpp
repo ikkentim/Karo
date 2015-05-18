2d0cf5e9c0f7c839aa040e7dda1a27dfd4ce5cd9
getro <lucasbudai@gmail.com>
2015-05-18 10:57:02 +0200

    AI improved

--- src/Karo.AI/Intelligence.cpp
+++ src/Karo.AI/Intelligence.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Intelligence.h"
+#include <stdio.h>
 
 using namespace std;
 
@@ -67,17 +68,21 @@ int Intelligence::evaluate(BoardState * state, BoardPlayer player) {
     if (state->piece_count() < PIECE_COUNT)
         return 0;
 
-    for (int i = 0; i < PIECE_COUNT; i++) {
-        //for each piece
-        //is piece flipped
-        if (!allPieces[i].is_face_up)
-            continue;
+	for (int i = 0; i < PIECE_COUNT; i++) {
+		//for each piece
+		//is piece flipped
+		if (!allPieces[i].is_face_up)
+			continue;
 
-        bool mypiece = (allPieces[i].player == player);
+	//	bool mypiece = (allPieces[i].player == player);
 
-        BoardTile tile = allPieces[i].tile;
+	//	BoardTile tile = allPieces[i].tile;
 
-#define TRY_OFFSET(ox,oy, idx); int idx = state->row_length(tile.x, tile.y, (ox), (oy), player) + \
+		score += best_score(state, player, allPieces, i);
+	}
+//old Macro
+/*
+	#define TRY_OFFSET(ox,oy, idx); int idx = state->row_length(tile.x, tile.y, (ox), (oy), player) + \
             1 + \
             state->row_length(tile.x, tile.y, -(ox), -(oy), player); \
             if(idx >= 4) { return mypiece ? MAX_SCORE : MIN_SCORE; } else { score += mypiece ? idx : -idx; }
@@ -89,6 +94,63 @@ int Intelligence::evaluate(BoardState * state, BoardPlayer player) {
 
 #undef TRY_OFFSET
     }
-
+	*/
     return score;
+}
+
+int Intelligence::best_score(BoardState * state, BoardPlayer player, BoardPiece* allPieces, int i){
+
+
+	int idx = 0;
+	bool mypiece = (allPieces[i].player == player);
+
+	BoardTile tile = allPieces[i].tile;
+
+	//Array who store all diagonal
+	int diag[2][4];
+	//Diag1
+	diag[0][0] = diag[1][0] = 1;
+	//diag2
+	diag[0][1] = 1;
+	diag[1][1] = 0;
+	//diag3
+	diag[0][2] = 0;
+	diag[1][2] = 1;
+	//diag4
+	diag[0][3] = -1;
+	diag[1][3] = 1;
+
+	
+	for (int j=0; j<4; j++){
+		//Calculate the lenght of the line
+		int lenA = state->row_length(tile.x, tile.y, diag[0][j], diag[1][j], allPieces[i].player);
+		
+		int lenB = state->row_length(tile.x, tile.y, -diag[0][j], -diag[1][j], allPieces[i].player);
+
+		idx += lenA+1+lenB;
+		
+		if (idx >= 4){
+			return mypiece ? MAX_SCORE : MIN_SCORE;
+		}
+		
+		
+		//Check if the edge of the line is blocked by an enemy piece or not
+		bool blockedA = state->piece(tile.x + diag[0][j] + lenA * diag[0][j], tile.y + diag[1][j] + lenA * diag[1][j], NULL);
+		bool blockedB = state->piece(tile.x  -diag[0][j] - lenB * diag[0][j], tile.y - diag[1][j] - lenB * diag[1][j], NULL);
+		
+		//if it's blocked we decrease the score
+		if (blockedA){
+			idx--;
+		}
+		if (blockedB){
+			idx--;
+		}
+		if (blockedA && blockedB){
+			idx = 0;
+		}
+	}
+
+	return mypiece ? idx : -idx;
+
+
 }
